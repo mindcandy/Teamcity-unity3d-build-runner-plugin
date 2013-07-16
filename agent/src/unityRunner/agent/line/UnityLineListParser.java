a338ab079abd43aacab7154125322f568f6d9bee
Guy John <guy.john@mindcandy.com>
2013-07-16 12:16:16 +0100

    Removing import of FinalArrayList in UnityLineListParser
    Didn't seem to be used and was apparently causing issues on some builds

--- agent/src/unityRunner/agent/line/UnityLineListParser.java
+++ agent/src/unityRunner/agent/line/UnityLineListParser.java
@@ -8,7 +8,6 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.ParserConfigurationException;
 
-import com.sun.istack.internal.FinalArrayList;
 import org.w3c.dom.Document;
 import org.w3c.dom.NodeList;
 import org.w3c.dom.Node;
